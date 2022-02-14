79ab81cfd89615995418aa1b2951105c0954acee
andrescas015 <Castexandrex015@gmail.com>
2022-02-14 18:53:20 -0500

    Update proveedores.java

--- src/agro2/empleadosBl/proveedores.java
+++ src/agro2/empleadosBl/proveedores.java
@@ -10,5 +10,47 @@ package agro2.empleadosBl;
  * @author SENA
  */
 public class proveedores {
-    
+    int Nit, Telefono;
+    String Direccion, correo, Rsocial;
+
+    public int getNit() {
+        return Nit;
+    }
+
+    public void setNit(int Nit) {
+        this.Nit = Nit;
+    }
+
+    public int getTelefono() {
+        return Telefono;
+    }
+
+    public void setTelefono(int Telefono) {
+        this.Telefono = Telefono;
+    }
+
+    public String getDireccion() {
+        return Direccion;
+    }
+
+    public void setDireccion(String Direccion) {
+        this.Direccion = Direccion;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getRsocial() {
+        return Rsocial;
+    }
+
+    public void setRsocial(String Rsocial) {
+        this.Rsocial = Rsocial;
+    }
+     
 }
