74fc6d6d6a270b083887a61b568309618c1053be
Javier Pacheco <javier.709@hotmail.com>
2022-02-14 20:33:35 -0500

    Ventana Empleados

---
+++ src/agroo/empleadosBl/empleadosBl.java
@@ -0,0 +1,34 @@
+
+package agroo.empleadosBl;
+
+
+public class empleadosBl {
+    
+    int ID;
+    String Nombre, Correo;
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getCorreo() {
+        return Correo;
+    }
+
+    public void setCorreo(String Correo) {
+        this.Correo = Correo;
+    }
+    
+}
