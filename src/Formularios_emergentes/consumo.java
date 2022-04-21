585693752093ea1d79bc4603a724482709d25f9b
josevalerocely <jrvalero7@misena.edu.co>
2022-04-20 21:12:58 -0500

    20-04-22-21:12

--- src/Formularios_emergentes/consumo.java
+++ src/Formularios_emergentes/consumo.java
@@ -1,12 +1,5 @@
 package Formularios_emergentes;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
 import java.awt.BorderLayout;
 import java.sql.Connection;
 import java.sql.DriverManager;
