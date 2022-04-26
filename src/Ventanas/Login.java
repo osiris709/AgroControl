7163562aef983f6025e561f41e98e8ec3aeb5261
Javier Pacheco <javier.709@hotmail.com>
2022-04-26 15:01:01 -0500

    Update Login.java

--- src/Ventanas/Login.java
+++ src/Ventanas/Login.java
@@ -21,7 +21,7 @@ public class Login extends javax.swing.JFrame {
 
     public Login() {
         initComponents();
-        this.setTitle("AgroControl");
+        this.setTitle("AgroControl - Inicio");
         this.setLocationRelativeTo(null);
         setResizable(false);
     }
